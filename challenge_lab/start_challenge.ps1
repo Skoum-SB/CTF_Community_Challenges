80d43d2c75eaadc3ec687d62d04485bf3b1dc8cf
Simon Bensiam <sbensiam@gmail.com>
2025-04-29 20:43:34 +0200

    Encore une update

--- challenge_lab/start_challenge.ps1
+++ challenge_lab/start_challenge.ps1
@@ -1,8 +1,8 @@
-$existingContainer = docker ps -q --filter ancestor=skoum/ctf_community_challenge_web
+$existingContainer = docker ps -q --filter ancestor=skoum/ctf-lab-services
 if ($existingContainer) {
     docker stop $existingContainer
 }
 
-docker run -d -p 5000:5000 skoum/ctf_community_challenge_web
+docker run -d -p 80:80 -p 21:21 -p 20:20 -p 21000-21010:21000-21010 -p 22:22 skoum/ctf-lab-services
 
-Write-Host "Le challenge a démarré sur http://localhost:5000. Bonne chance !"
+Write-Host "Le challenge a démarré. Bonne chance !"
--- challenge_lab/start_challenge.sh
+++ challenge_lab/start_challenge.sh
@@ -1,8 +1,8 @@
-EXISTING_CONTAINER=$(docker ps -q --filter ancestor=skoum/ctf_community_challenge_mc)
+EXISTING_CONTAINER=$(docker ps -q --filter ancestor=skoum/ctf-lab-services)
 if [ ! -z "$EXISTING_CONTAINER" ]; then
     docker stop $EXISTING_CONTAINER
 fi
 
-docker run -d -p 5000:5000 skoum/ctf_community_challenge_mc
+docker run -d -p 80:80 -p 21:21 -p 20:20 -p 21000-21010:21000-21010 -p 22:22 skoum/ctf-lab-services
 
-echo "Le challenge a démarré sur http://localhost:5000. Bonne chance !"
+echo "Le challenge a démarré. Bonne chance !"
