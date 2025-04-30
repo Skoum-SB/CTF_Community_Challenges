9b164ac4b6141379474c4d5454ee528f7c530fcc
Simon Bensiam <sbensiam@gmail.com>
2025-04-30 12:06:20 +0200

    Update scripts

--- challenge_lab/start_challenge.ps1
+++ challenge_lab/start_challenge.ps1
@@ -1,8 +1,24 @@
-$existingContainer = docker ps -q --filter ancestor=skoum/ctf-lab-services
-if ($existingContainer) {
-    docker stop $existingContainer
+# Arrêter les conteneurs existants s'ils existent
+$existingServices = docker ps -q --filter ancestor=skoum/ctf-lab-services
+if ($existingServices) {
+    docker stop $existingServices
 }
 
-docker run -d -p 80:80 -p 21:21 -p 20:20 -p 21000-21010:21000-21010 -p 22:22 skoum/ctf-lab-services
+$existingKali = docker ps -q --filter ancestor=skoum/ctf-lab-kali
+if ($existingKali) {
+    docker stop $existingKali
+}
 
-Write-Host "Le challenge a démarré. Bonne chance !"
+# Créer le réseau Docker s'il n'existe pas déjà
+docker network inspect ctf-network > $null 2>&1
+if (-not $?) {
+    docker network create --subnet=172.20.0.0/24 ctf-network
+}
+
+# Lancer le conteneur de services CTF
+docker run -d --name ctf-lab-services --network ctf-network --ip 172.20.0.2 -p 80:80 -p 21:21 -p 20:20 -p 21000-21010:21000-21010 -p 22:22 skoum/ctf-lab-services
+
+# Lancer le conteneur Kali
+docker run -d --name ctf-lab-kali --network ctf-network --ip 172.20.0.3 -p 1337:22 --cap-add NET_ADMIN --cap-add SYS_ADMIN skoum/ctf-lab-kali
+
+Write-Host "Le challenge a démarré ! La machine Kali est accessible via SSH sur le port 1337. Bonne chance !"
--- challenge_lab/start_challenge.sh
+++ challenge_lab/start_challenge.sh
@@ -1,8 +1,25 @@
-EXISTING_CONTAINER=$(docker ps -q --filter ancestor=skoum/ctf-lab-services)
-if [ ! -z "$EXISTING_CONTAINER" ]; then
-    docker stop $EXISTING_CONTAINER
+#!/bin/bash
+
+# Arrêter les conteneurs existants s'ils existent
+EXISTING_SERVICES=$(docker ps -q --filter ancestor=skoum/ctf-lab-services)
+if [ ! -z "$EXISTING_SERVICES" ]; then
+    docker stop $EXISTING_SERVICES
 fi
 
-docker run -d -p 80:80 -p 21:21 -p 20:20 -p 21000-21010:21000-21010 -p 22:22 skoum/ctf-lab-services
+EXISTING_KALI=$(docker ps -q --filter ancestor=skoum/ctf-lab-kali)
+if [ ! -z "$EXISTING_KALI" ]; then
+    docker stop $EXISTING_KALI
+fi
 
-echo "Le challenge a démarré. Bonne chance !"
+# Créer le réseau Docker s'il n'existe pas déjà
+if ! docker network inspect ctf-network &>/dev/null; then
+    docker network create --subnet=172.20.0.0/24 ctf-network
+fi
+
+# Lancer le conteneur de services CTF
+docker run -d --name ctf-lab-services --network ctf-network --ip 172.20.0.2 -p 80:80 -p 21:21 -p 20:20 -p 21000-21010:21000-21010 -p 22:22 skoum/ctf-lab-services
+
+# Lancer le conteneur Kali
+docker run -d --name ctf-lab-kali --network ctf-network --ip 172.20.0.3 -p 1337:22 --cap-add NET_ADMIN --cap-add SYS_ADMIN skoum/ctf-lab-kali
+
+echo "Le challenge a démarré ! La machine Kali est accessible via SSH sur le port 1337. Bonne chance !"
